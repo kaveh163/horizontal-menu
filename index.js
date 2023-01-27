214776d9f7047dbee5cbd37a0b5dc1a9b1b9d33b
Kaveh <kavehfrounchi@gmail.com>
2023-01-27 10:12:05 -0500

    Finished menu view on mobile device

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 let toggleMenu = document.querySelector(".active");
 let dropDownMenu = document.querySelector(".dropMenu ul:nth-child(2)");
 let toggleMainMenu = document.querySelector("nav div:nth-child(1) i");
+let toggleAbout = document.querySelector(".menu .dropMenu .active");
 let mainMenu = document.querySelector(".menu");
 let header  = document.querySelector("header");
 let is_menu = false;
 toggleMenu.onclick = function() {
     dropDownMenu.classList.toggle('dropMenu__List');
+    toggleAbout.classList.toggle('toggleAbout');
     // header.classList.toggle("headerToggle__height");
 }
 toggleMainMenu.onclick = function() {
     is_menu = !is_menu;
     // mainMenu.classList.toggle('menu__Toggle');
-    header.classList.toggle("headerToggle");
+    // header.classList.toggle("headerToggle");
     // header.classList.toggle("headerToggle__height");
     if(is_menu) {
         header.classList.add("headerToggle");
     } else {
        
         dropDownMenu.classList.remove("dropMenu__List");
+        toggleAbout.classList.remove("toggleAbout");
         header.classList.remove("headerToggle");
     }
 }
