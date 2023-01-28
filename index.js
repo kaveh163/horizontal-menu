51057b5b670cfc943c79b8e8cace0b1abc17d268
Kaveh <kavehfrounchi@gmail.com>
2023-01-28 10:49:27 -0500

    Updated the main menu icon to toggle to another icon

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ let dropDownMenu = document.querySelector(".dropMenu ul:nth-child(2)");
 let toggleMainMenu = document.querySelector("nav div:nth-child(1) i");
 let toggleAbout = document.querySelector(".menu .dropMenu .active");
 let mainMenu = document.querySelector(".menu");
-let header  = document.querySelector("header");
+let header = document.querySelector("header");
+let bars = document.querySelector(".fa-bars");
+let xmark = document.querySelector(".fa-xmark");
 let is_menu = false;
-toggleMenu.onclick = function() {
-    dropDownMenu.classList.toggle('dropMenu__List');
-    toggleAbout.classList.toggle('toggleAbout');
-    // header.classList.toggle("headerToggle__height");
+toggleMenu.onclick = function () {
+  dropDownMenu.classList.toggle("dropMenu__List");
+  toggleAbout.classList.toggle("toggleAbout");
+  // header.classList.toggle("headerToggle__height");
 }
-toggleMainMenu.onclick = function() {
-    is_menu = !is_menu;
-    // mainMenu.classList.toggle('menu__Toggle');
-    // header.classList.toggle("headerToggle");
-    // header.classList.toggle("headerToggle__height");
-    if(is_menu) {
-        header.classList.add("headerToggle");
-    } else {
-       
-        dropDownMenu.classList.remove("dropMenu__List");
-        toggleAbout.classList.remove("toggleAbout");
-        header.classList.remove("headerToggle");
-    }
-}
+
+$(bars).on("click", function () {
+  header.classList.add("headerToggle");
+  bars.style.display = "none";
+  $(".fa-xmark").fadeIn(1000);
+});
+$(xmark).on("click", function () {
+  $(".fa-bars").fadeIn(2500);
+  xmark.style.display = "none";
+  dropDownMenu.classList.remove("dropMenu__List");
+  toggleAbout.classList.remove("toggleAbout");
+  header.classList.remove("headerToggle");
+});
