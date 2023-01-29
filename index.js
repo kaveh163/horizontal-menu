5d4845c3f6a4acbbf6bb3f5d2d88b8a332c8cd61
Kaveh <kavehfrounchi@gmail.com>
2023-01-29 09:03:02 -0500

    updated the toggle icon css

--- index.js
+++ index.js
@@ -11,17 +11,43 @@ toggleMenu.onclick = function () {
   dropDownMenu.classList.toggle("dropMenu__List");
   toggleAbout.classList.toggle("toggleAbout");
   // header.classList.toggle("headerToggle__height");
-}
+};
+// toggle either with classes or elements:
 
-$(bars).on("click", function () {
-  header.classList.add("headerToggle");
-  bars.style.display = "none";
-  $(".fa-xmark").fadeIn(1000);
-});
-$(xmark).on("click", function () {
-  $(".fa-bars").fadeIn(2500);
-  xmark.style.display = "none";
-  dropDownMenu.classList.remove("dropMenu__List");
-  toggleAbout.classList.remove("toggleAbout");
-  header.classList.remove("headerToggle");
-});
+// toggle with classes
+toggleMainMenu.onclick = function () {
+  is_menu = !is_menu;
+  if (is_menu) {
+    header.classList.add("headerToggle");
+    toggleMainMenu.classList.add("hide");
+    toggleMainMenu.addEventListener("transitionend", () => {
+        toggleMainMenu.classList.remove('hide');
+        toggleMainMenu.classList.remove('fa-bars');
+        toggleMainMenu.classList.add('fa-xmark');
+    })
+  } else {
+    toggleMainMenu.classList.add("hide");
+    toggleMainMenu.addEventListener("transitionend", function() {
+        toggleMainMenu.classList.remove('hide');
+        toggleMainMenu.classList.remove('fa-xmark');
+        toggleMainMenu.classList.add('fa-bars');
+    })
+    dropDownMenu.classList.remove("dropMenu__List");
+    toggleAbout.classList.remove("toggleAbout");
+    header.classList.remove("headerToggle");
+  }
+};
+// toggle with elements
+
+// $(bars).on("click", function () {
+//   header.classList.add("headerToggle");
+//   bars.style.display = "none";
+//   $(".fa-xmark").fadeIn(1000);
+// });
+// $(xmark).on("click", function () {
+//   $(".fa-bars").fadeIn(2500);
+//   xmark.style.display = "none";
+//   dropDownMenu.classList.remove("dropMenu__List");
+//   toggleAbout.classList.remove("toggleAbout");
+//   header.classList.remove("headerToggle");
+// });
